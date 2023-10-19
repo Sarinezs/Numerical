f45497d2ae76cf0319b137ac7ea99f3b5cc06006
Sarinezs <a.edr.edm.ed@gmail.com>
2023-10-19 12:47:38 +0700

    .

--- React/my-app/src/NavBar.js
+++ React/my-app/src/NavBar.js
@@ -9,43 +9,44 @@ export const NavBar = () =>{
             <Navbar key={expand} bg="dark"variant="dark" expand={expand} className="mb-3">
               <Container fluid>
                 <Navbar.Brand >Numer Project</Navbar.Brand>
-                <NavDropdown.Divider />
-                
 
-                {/* <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />   */}
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />  
 
-                <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`}
-                  aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement="end">
+                <Navbar.Offcanvas
+                  id={`offcanvasNavbar-expand-${expand}`}
+                  aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                  placement="end">
+
+                  <Offcanvas.Header closeButton>
+                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                      Menu
+                    </Offcanvas.Title>
+                  </Offcanvas.Header>
 
                   <Offcanvas.Body>
                     <Nav className="justify-content-end flex-grow-1 pe-3">
                     
                     <NavDropdown
-                        title="Root Of Equation"
-                        id={`offcanvasNavbarDropdown-expand-${expand}`}>
-                        <NavDropdown.Item href="/Bisection">Bisection</NavDropdown.Item>
-                        <br/>
-                        <NavDropdown.Item href="/FalsePosition">FalsePosition</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                      </NavDropdown>
+                          title="Root Of Equation"
+                          id={`offcanvasNavbarDropdown-expand-${expand}`}>
+                          <NavDropdown.Item href="/Bisection">Bisection</NavDropdown.Item><NavDropdown.Divider />
+                          <NavDropdown.Item href="/FalsePosition">FalsePosition</NavDropdown.Item>
 
-                      <NavDropdown
-                        title="Interpolation"
-                        id={`offcanvasNavbarDropdown-expand-${expand}`}>
-                        <NavDropdown.Item href="/Newton">Newton</NavDropdown.Item>
-                        <br/>
-                        <NavDropdown.Item href="/Lagrange">Lagrange</NavDropdown.Item><NavDropdown.Divider />
-                      </NavDropdown>
+                        </NavDropdown>
 
-                      <NavDropdown
-                        title="Solutions Root of equation"
-                        id={`offcanvasNavbarDropdown-expand-${expand}`}>
-                        <NavDropdown.Item href="/Gauss_elimination">Gauss_elimination</NavDropdown.Item>
-                        <br/>
-                        <NavDropdown.Item href="/Gauss_Jordan">Gauss_Jordan</NavDropdown.Item><NavDropdown.Divider />
-                        <br/>
-                      </NavDropdown>
+                        <NavDropdown
+                          title="Interpolation"
+                          id={`offcanvasNavbarDropdown-expand-${expand}`}>
+                          <NavDropdown.Item href="/Newton">Newton</NavDropdown.Item><NavDropdown.Divider />
+                          <NavDropdown.Item href="/Lagrange">Lagrange</NavDropdown.Item>
+                        </NavDropdown>
 
+                        <NavDropdown
+                          title="Solutions Root of equation"
+                          id={`offcanvasNavbarDropdown-expand-${expand}`}>
+                          <NavDropdown.Item href="/Gauss_elimination">Gauss_elimination</NavDropdown.Item> <NavDropdown.Divider />
+                          <NavDropdown.Item href="/Gauss_Jordan">Gauss_Jordan</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
                     
                   </Offcanvas.Body>
